29b3b53b79cb31ca896c2ed1ffe3f84ea3e8a44d
niewei_work <niewei@huizhaofang.com>
2019-03-19 11:04:34 +0800

    add init

---
+++ app/src/main/java/com/neville/moduletest/myapplication/sort/SelectionSort.java
@@ -0,0 +1,14 @@
+package com.neville.moduletest.myapplication.sort;
+
+/**
+ * Created by niewei on 2018/9/26.
+ */
+
+public class SelectionSort {
+
+
+    public static void main(String[] args) {
+     String phone="342  432432 432 432";
+     System.out.println(phone.replaceAll(" ",""));
+    }
+}
