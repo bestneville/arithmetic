0fc0bd30008f82ec18828240f3a9b5a6956338e7
niewei_work <niewei@huizhaofang.com>
2019-06-19 11:39:48 +0800

    add 图--无向图

---
+++ app/src/main/java/com/neville/moduletest/myapplication/dataStructure/graph/Graph.java
@@ -0,0 +1,35 @@
+package com.neville.moduletest.myapplication.dataStructure.graph;
+
+import java.util.LinkedList;
+
+/**
+ * Created by niewei on 2019/6/13.
+ */
+
+public class Graph {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    //无向图
+    public static class MyGraph {
+        private int v;
+        private LinkedList<Integer>[] adj;
+
+        public MyGraph(int v) {
+            this.v = v;
+            adj = new LinkedList[v];
+            for (int i = 0; i < v; i++) {
+                adj[i] = new LinkedList<>();
+            }
+        }
+
+        private void addEdge(int s, int t) {
+            //无向图一条边存两次
+            adj[s].add(t);
+            adj[t].add(s);
+        }
+    }
+}
---
+++ app/src/main/java/com/neville/moduletest/myapplication/search/BreadthFirstSearch.java
@@ -0,0 +1,20 @@
+package com.neville.moduletest.myapplication.search;
+
+import com.neville.moduletest.myapplication.dataStructure.graph.Graph;
+
+/**
+ * Created by niewei on 2019/6/13.
+ */
+
+//广度优先搜索
+public class BreadthFirstSearch {
+
+    public static void main(String[] args) {
+        Graph.MyGraph graph = new Graph.MyGraph(8);
+    }
+
+    public void bfs(int s, int t) {
+
+    }
+
+}
